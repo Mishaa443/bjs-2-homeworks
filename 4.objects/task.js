5749278d008c109fb357455d86589958831a0f97
Kardapotsev Mihail <Misha443@bk.ru>
2022-08-06 17:59:24 +0500

    task 4

--- 4.objects/task.js
+++ 4.objects/task.js
@@ -1,10 +1,62 @@
 function Student(name, gender, age) {
-    // Ваш код
+  this.name = name
+  this.gender = gender
+  this.age = age
 
 }
 
-Student.prototype.setSubject = function (subjectName) {
-  //ваш код
+Student.prototype.setSubject = function(subjectName) {
+  this.subject = subjectName
+}
+Student.prototype.addMark = function(mark) {
+  if(this.marks === undefined){ 
+    this.marks = [mark]
+    } else {
+      this.marks.push(mark)
+    }
 }
 
-// ваш код для остальных методов
+Student.prototype.addMarks = function(...mark) {
+  if(this.marks === undefined){ 
+    this.marks = mark
+    } else {
+      for( let i = 0; i < mark.length; i++){
+        this.marks.push(mark[i])
+      }
+      
+    }
+}
+
+Student.prototype.getAverage = function(){
+  let sum = 0
+      sum = this.marks.reduce((a, b) => a + b, 0)
+  
+   return sum / this.marks.length
+}
+
+Student.prototype.exclude = function(reason){
+    delete student1.subject
+    delete student1.marks
+    this.excluded = reason
+}
+
+
+
+
+
+let student1 = new Student("Tony", "male", 37);
+student1.setSubject("Algebra");
+student1.addMark(5);
+student1.addMark(4);
+student1.addMark(5);
+student1.getAverage();
+console.log(student1);
+// {age: 37, gender: "male", marks: [5, 4, 5], name: "Tony", subject: "Algebra"}
+let student2 = new Student("Buzz", "female", 35);
+student2.setSubject("Geometry");
+student2.addMark(2);
+student2.addMark(3);
+student2.addMark(2);
+student2.exclude('low grades')
+console.log(student2)
+// {name: "Buzz", gender: "female", age: 35, excluded: "low grades"}
